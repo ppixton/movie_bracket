e45ed702c992946dc6e9b22a2918c12a7c62cdd3
Parker Pixton <parker.pixton@gmail.com>
2023-07-25 19:39:18 -0600

    removed password for v1

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,6 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
   TextEditingController userController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -64,29 +63,12 @@ class _LoginState extends State<Login> {
               ),
               Padding(
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextFormField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), labelText: "Password"),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding:
                     const EdgeInsets.symmetric(horizontal: 8, vertical: 16.0),
                 child: Center(
                   child: ElevatedButton(
                     onPressed: () {
                       if (_formKey.currentState!.validate()) {
-                        if (userController.text == "parker" &&
-                            passwordController.text == "password") {
+                        if (userController.text == "parker") {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
